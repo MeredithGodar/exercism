bb2f2f99986ebc4c7608395901ed927e774ea7b0
Meredith Godar <meredithgodar@iseinc.biz>
2017-02-02 16:24:50 -0600

    completed python pangram exercise

---
+++ python/pangram/pangram.py
@@ -0,0 +1,28 @@
+def is_pangram(sentence):
+    lower_case_sentence = sentence.lower()
+    return 'a' in lower_case_sentence and \
+           'b' in lower_case_sentence and \
+           'c' in lower_case_sentence and \
+           'd' in lower_case_sentence and \
+           'e' in lower_case_sentence and \
+           'f' in lower_case_sentence and \
+           'g' in lower_case_sentence and \
+           'h' in lower_case_sentence and \
+           'i' in lower_case_sentence and \
+           'j' in lower_case_sentence and \
+           'k' in lower_case_sentence and \
+           'l' in lower_case_sentence and \
+           'm' in lower_case_sentence and \
+           'n' in lower_case_sentence and \
+           'o' in lower_case_sentence and \
+           'p' in lower_case_sentence and \
+           'q' in lower_case_sentence and \
+           'r' in lower_case_sentence and \
+           's' in lower_case_sentence and \
+           't' in lower_case_sentence and \
+           'u' in lower_case_sentence and \
+           'v' in lower_case_sentence and \
+           'w' in lower_case_sentence and \
+           'x' in lower_case_sentence and \
+           'y' in lower_case_sentence and \
+           'z' in lower_case_sentence
