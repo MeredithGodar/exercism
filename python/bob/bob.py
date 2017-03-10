0f54eff6a6878eb8d18c6cf8c0281b0a031e4d55
Meredith Godar <meredith.godar@gmail.com>
2017-03-09 21:01:29 -0600

    python bob exercise completed

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,29 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
+# -*- coding: utf-8 -*-
+
+import re
+import unicodedata
 
 
 def hey(what):
 
-    return
+    if what.strip() == '':
+        return 'Fine. Be that way!'
+    elif what.strip().endswith('?') and not is_shouting(what):
+        return 'Sure.'
+    elif is_shouting(what):
+        response = 'Whoa, chill out!'
+
+        for c in what:
+            if unicodedata.category(c) == 'Ll':
+                response = 'Whatever.'
+
+        return response
+    else:
+        return 'Whatever.'
+
+
+def is_shouting(what):
+    alpha_character_substring = re.sub(r'[^a-zA-Z]', '', what)
+    uppercase_substring = [l for l in alpha_character_substring if l.isupper()]
+
+    return len(uppercase_substring) == len(alpha_character_substring) and len(uppercase_substring) != 0
